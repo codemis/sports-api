277c3d9acc3bb147d35170600310d102e6b7a0a6
Codemis <johnathan@jpulos.com>
2025-10-26 15:46:29 -0700

    Added license.

--- src/parsers/nfl-parser.js
+++ src/parsers/nfl-parser.js
@@ -1,3 +1,21 @@
+/**
+ * Sports API - A script that pulls sports event data from SportsDB API
+ * Copyright (C) 2025 Johnathan Pulos
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { formatEventDateTime } from '../utilities.js';
 
 /**
